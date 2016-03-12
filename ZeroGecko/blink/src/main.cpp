a4dca4db1d8fc7678af86b15b007b08ccfd4292f
Caspar Friedrich <casparfriedrich@Caspars-MacBook-Pro.fritz.box>
2016-03-12 21:44:16 +0100

    ...

--- ZeroGecko/blink/src/main.cpp
+++ ZeroGecko/blink/src/main.cpp
@@ -7,27 +7,27 @@
 static volatile uint32_t msTicks;
 
 static void Delay(uint32_t dlyTicks) {
-  uint32_t curTicks = msTicks;
-  while ((msTicks - curTicks) < dlyTicks)
-    ;
+    uint32_t curTicks = msTicks;
+    while ((msTicks - curTicks) < dlyTicks) {
+    }
 }
 
 void SysTick_Handler(void) { msTicks++; }
 
 int main(void) {
-  CHIP_Init();
+    CHIP_Init();
 
-  SysTick_Config(CMU_ClockFreqGet(cmuClock_CORE) / 1000);
+    SysTick_Config(CMU_ClockFreqGet(cmuClock_CORE) / 1000);
 
-  CMU_ClockEnable(cmuClock_GPIO, true);
+    CMU_ClockEnable(cmuClock_GPIO, true);
 
-  GPIO_PinModeSet(gpioPortC, 10, gpioModePushPull, 0);
-  GPIO_PinModeSet(gpioPortC, 11, gpioModePushPull, 0);
+    GPIO_PinModeSet(gpioPortC, 10, gpioModePushPull, 0);
+    GPIO_PinModeSet(gpioPortC, 11, gpioModePushPull, 0);
 
-  while (1) {
-    GPIO_PinOutToggle(gpioPortC, 10);
-    Delay(250);
-    GPIO_PinOutToggle(gpioPortC, 11);
-    Delay(250);
-  }
+    while (1) {
+        GPIO_PinOutToggle(gpioPortC, 10);
+        Delay(250);
+        GPIO_PinOutToggle(gpioPortC, 11);
+        Delay(250);
+    }
 }
--- ZeroGecko/gpiointerrupt_freertos/src/main.cpp
+++ ZeroGecko/gpiointerrupt_freertos/src/main.cpp
@@ -12,84 +12,82 @@
 #include "task.h"
 
 typedef struct {
-	GPIO_Port_TypeDef port;
-	unsigned int pin;
-	SemaphoreHandle_t semaphore;
+    GPIO_Port_TypeDef port;
+    unsigned int pin;
+    SemaphoreHandle_t semaphore;
 } Task0_Parameter_TypeDef;
 
 typedef struct {
-	GPIO_Port_TypeDef port;
-	unsigned int pin;
-	SemaphoreHandle_t semaphore;
+    GPIO_Port_TypeDef port;
+    unsigned int pin;
+    SemaphoreHandle_t semaphore;
 } Task1_Parameter_TypeDef;
 
 Task0_Parameter_TypeDef parametersToTask0;
 Task1_Parameter_TypeDef parametersToTask1;
 
 void gpioCallback(uint8_t pin) {
-	switch (pin) {
-	case 8: {
-		xSemaphoreGiveFromISR(parametersToTask1.semaphore, NULL);
-		break;
-	}
-	case 9: {
-		xSemaphoreGiveFromISR(parametersToTask0.semaphore, NULL);
-		break;
-	}
-	}
+    switch (pin) {
+        case 8: {
+            xSemaphoreGiveFromISR(parametersToTask1.semaphore, NULL);
+        } break;
+        case 9: {
+            xSemaphoreGiveFromISR(parametersToTask0.semaphore, NULL);
+        } break;
+    }
 }
 
-void taskLed0(void *pParameters) {
-	Task0_Parameter_TypeDef *pData = (Task0_Parameter_TypeDef *) pParameters;
-	for (;;) {
-		if (xSemaphoreTake(pData->semaphore, 0) == pdTRUE) {
-			GPIO_PinOutToggle(pData->port, pData->pin);
-		}
-	}
+void taskLed0(void* pParameters) {
+    Task0_Parameter_TypeDef* pData = (Task0_Parameter_TypeDef*)pParameters;
+    for (;;) {
+        if (xSemaphoreTake(pData->semaphore, 0) == pdTRUE) {
+            GPIO_PinOutToggle(pData->port, pData->pin);
+        }
+    }
 }
 
-void taskLed1(void *pParameters) {
-	Task1_Parameter_TypeDef *pData = (Task1_Parameter_TypeDef *) pParameters;
-	for (;;) {
-		if (xSemaphoreTake(pData->semaphore, 0) == pdTRUE) {
-			GPIO_PinOutToggle(pData->port, pData->pin);
-		}
-	}
+void taskLed1(void* pParameters) {
+    Task1_Parameter_TypeDef* pData = (Task1_Parameter_TypeDef*)pParameters;
+    for (;;) {
+        if (xSemaphoreTake(pData->semaphore, 0) == pdTRUE) {
+            GPIO_PinOutToggle(pData->port, pData->pin);
+        }
+    }
 }
 
 int main(void) {
-	CHIP_Init();
+    CHIP_Init();
 
-	CMU_ClockEnable(cmuClock_GPIO, true);
+    CMU_ClockEnable(cmuClock_GPIO, true);
 
-	GPIO_PinModeSet(gpioPortC, 8, gpioModeInput, 0);
-	GPIO_IntConfig(gpioPortC, 8, false, true, true);
+    GPIO_PinModeSet(gpioPortC, 8, gpioModeInput, 0);
+    GPIO_IntConfig(gpioPortC, 8, false, true, true);
 
-	GPIO_PinModeSet(gpioPortC, 9, gpioModeInput, 0);
-	GPIO_IntConfig(gpioPortC, 9, false, true, true);
+    GPIO_PinModeSet(gpioPortC, 9, gpioModeInput, 0);
+    GPIO_IntConfig(gpioPortC, 9, false, true, true);
 
-	GPIO_PinModeSet(gpioPortC, 10, gpioModePushPullDrive, 0);
-	GPIO_PinModeSet(gpioPortC, 11, gpioModePushPullDrive, 0);
+    GPIO_PinModeSet(gpioPortC, 10, gpioModePushPullDrive, 0);
+    GPIO_PinModeSet(gpioPortC, 11, gpioModePushPullDrive, 0);
 
-	GPIOINT_CallbackRegister(8, gpioCallback);
-	GPIOINT_CallbackRegister(9, gpioCallback);
+    GPIOINT_CallbackRegister(8, gpioCallback);
+    GPIOINT_CallbackRegister(9, gpioCallback);
 
-	parametersToTask0.port = gpioPortC;
-	parametersToTask0.pin = 10;
-	parametersToTask0.semaphore = xSemaphoreCreateBinary();
+    parametersToTask0.port = gpioPortC;
+    parametersToTask0.pin = 10;
+    parametersToTask0.semaphore = xSemaphoreCreateBinary();
 
-	parametersToTask1.port = gpioPortC;
-	parametersToTask1.pin = 11;
-	parametersToTask1.semaphore = xSemaphoreCreateBinary();
+    parametersToTask1.port = gpioPortC;
+    parametersToTask1.pin = 11;
+    parametersToTask1.semaphore = xSemaphoreCreateBinary();
 
-	xTaskCreate(taskLed0, NULL, configMINIMAL_STACK_SIZE, &parametersToTask0,
-			tskIDLE_PRIORITY + 1, NULL);
-	xTaskCreate(taskLed1, NULL, configMINIMAL_STACK_SIZE, &parametersToTask1,
-			tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(taskLed0, NULL, configMINIMAL_STACK_SIZE, &parametersToTask0,
+                tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(taskLed1, NULL, configMINIMAL_STACK_SIZE, &parametersToTask1,
+                tskIDLE_PRIORITY + 1, NULL);
 
-	GPIOINT_Init();
+    GPIOINT_Init();
 
-	vTaskStartScheduler();
+    vTaskStartScheduler();
 
-	return 0;
+    return 0;
 }
