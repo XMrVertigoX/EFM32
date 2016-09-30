b5a12c45cdc8aae5420f2de821feef105fe8ee6b
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2016-09-30 11:33:58 +0200

    Remove noise in config file

--- sleep/src/config/rtcdrv_config.h
+++ sleep/src/config/rtcdrv_config.h
@@ -1,13 +1,7 @@
-#ifndef __SILICON_LABS_RTCDRV_CONFIG_H__
-#define __SILICON_LABS_RTCDRV_CONFIG_H__
+#ifndef RTCDRV_CONFIG_H
+#define RTCDRV_CONFIG_H
 
-// Define the number of timers the application needs.
 #define EMDRV_RTCDRV_NUM_TIMERS (4)
-
-// Define to include wallclock functionality.
-// #define EMDRV_RTCDRV_WALLCLOCK_CONFIG
-
-// Define to enable integration with SLEEP driver.
 #define EMDRV_RTCDRV_SLEEPDRV_INTEGRATION
 
-#endif /* __SILICON_LABS_RTCDRV_CONFIG_H__ */
+#endif /* RTCDRV_CONFIG_H */
